cf0a8f3f4652f32cb1808fcfda92384c7c182163
Sajo8 <andrewsplin@protonmail.com>
2019-05-08 16:00:30 -0700

    make on_msg and commands work together
    teamwork is dreamwork

--- main.py
+++ main.py
@@ -1,49 +1,48 @@
 from github import Github
 from discord.ext import commands
-import traceback
 
-username = 'Soja8'
+# todo actually do the stuff bruh
 
 try:
+	username = 'Soja8'
 	password = open('password.txt').read()
-except:
-	print("Can't find password file, aborting.")
-	exit()
-
-try:
 	token = open('tokenfile.txt').read()
 except:
-	print("Can't find token file, aborting.")
+	print("Can't find password/token file, aborting.")
 	exit()
 
-g = Github(username, password)
+g = Github(username, password) # log into github my g
 
-
-
-bot = commands.Bot(command_prefix='!git ') # add a space in the prefix
+bot = commands.Bot(command_prefix='.git ') # add a space in the prefix
 bot.remove_command('help') # remove command so that we can make our own
+
+reserved_commands = ['help'] # list of the commands we make
+
+def msg_not_in_list_of_commands(ctx):
+	return str(ctx.command) not in reserved_commands # return true if the command passed is not in the list of reserved ones
 
 @bot.event
 async def on_ready():
 	channel = bot.get_channel(575153311259557915)
-	await channel.send('Github bot"s up')
+	await channel.send("Github bot's up") # when ready send a confirmation message
 
 @bot.command()
-async def help(ctx):
-	await ctx.send("lol i aint gonna help you")
+async def help(ctx): # help message on ".git help"
+	await ctx.send("Bot underworks, help message coming promptly!")
 
-
-"""
 @bot.event
 async def on_message(message):
 	
-	if message.author == bot.user:
+	if message.author == bot.user: # if the bot said it ignore
 		return
+	
+	ctx = await bot.get_context(message) # get context
 
-	channel = bot.get_channel(575153311259557915)
-
-	await channel.send(f'{message.author} said: {message.content}')
-"""
-
+	if msg_not_in_list_of_commands(ctx): # if command not in reserved ones
+		# just do whatever
+		channel = ctx.channel 
+		await channel.send(f'{ctx.author} said: {ctx.message.content}')
+	else:
+		if ctx.valid: await bot.invoke(ctx) # otherwise call appropriate command function(if valid)
 
 bot.run(token, bot=True)
